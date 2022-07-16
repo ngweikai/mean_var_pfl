cb8cd34913b895ce77964a85407dfe69c913c071
Wei Kai <ngweikai@gmail.com>
2022-07-16 14:25:08 +0800

    connects to get_hist_prices API with start & end time, to create df of hist_data.

--- mean_var_opt.py
+++ mean_var_opt.py
@@ -7,25 +7,27 @@ from datetime import datetime as dt
 API_KEY = 'LxIO7YyHJet0bVet36EVNv9ExkxjH3iqjOU9mNqq'
 API_SECRET = 'k9_GENAHBzWb8S51MEc1YOXd31ILmaLSe7B_fsqz'
 
+portfolio = ['BTC', 'ETH', 'SOL']
+market_type = 'PERP'
+
 epoch = dt(1970, 1, 1, 0, 0, 0)
 start = dt(2022, 6, 1, 0, 0, 0)
 end = dt(2022, 7, 1, 23, 0, 0)
-now = dt.now()
 
-# start.timestamp() returns GMT +8
+# start.timestamp() returns GMT +8, not UTC
 start_ts = int((start - epoch).total_seconds())
 end_ts = int((end - epoch).total_seconds())
 
-portfolio = ['BTC']
-market_type = 'PERP'
-
+hist_data = pd.DataFrame()
 api_conn = ftx_api.FtxApi(API_KEY, API_SECRET)
 for crypto in portfolio:
     name = f'{crypto}-{market_type}'
-    print(name)
-    prices = api_conn.get_hist_prices(name)
-    market = api_conn.get_market(name)
+    data = api_conn.get_hist_prices(name, 3600, start_ts, end_ts)
+    close_price = []
+    for hourly_data in data:
+        close_price.append(hourly_data['close'])
+    hist_data[name] = pd.Series(close_price)
 
-print(prices[-1])
-print(prices[-2])
-print(market['price'])
+print(hist_data.head())
+print(hist_data.tail())
+print(len(hist_data))
