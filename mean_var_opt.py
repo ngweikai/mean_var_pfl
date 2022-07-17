8ea90a59d9dca0ee1337c1aff7dc2d43b1a1fac6
Wei Kai <ngweikai@gmail.com>
2022-07-17 16:50:36 +0800

    cleaned up code.

--- mean_var_opt.py
+++ mean_var_opt.py
@@ -10,39 +10,42 @@ import ftx_api
 API_KEY = 'LxIO7YyHJet0bVet36EVNv9ExkxjH3iqjOU9mNqq'
 API_SECRET = 'k9_GENAHBzWb8S51MEc1YOXd31ILmaLSe7B_fsqz'
 
-portfolio = ['CEVA', 'GOOGL', 'TSLA', 'ZOM']
-#portfolio = ['ATOM', 'GMT']
+# portfolio = ['CEVA', 'GOOGL', 'TSLA', 'ZOM']
+portfolio = ['BTC', 'ETH', 'SOL']
 market_type = 'PERP'
 
 epoch = dt(1970, 1, 1, 0, 0, 0)
-start = dt(2021, 1, 1, 0, 0, 0)
-end = dt(2021, 1, 22, 0, 0, 0)
+start = dt(2022, 6, 1, 0, 0, 0)
+end = dt(2022, 7, 1, 23, 0, 0)
 
 # start.timestamp() returns GMT +8, not UTC
 start_ts = int((start - epoch).total_seconds())
 end_ts = int((end - epoch).total_seconds())
 
 hist_data = pd.DataFrame()
-for stock in portfolio:
-    close_data = web.DataReader(stock, 'yahoo', start, end)['Close']
-    hist_data[stock] = close_data
 
-"""
-api_conn = ftx_api.FtxApi(API_KEY, API_SECRET)
-for crypto in portfolio:
-    name = f'{crypto}-{market_type}'
-    data = api_conn.get_hist_prices(name, 3600, start_ts, end_ts)
-    close_price = []
-    for hourly_data in data:
-        close_price.append(hourly_data['close'])
-    hist_data[name] = pd.Series(close_price)
-"""
+def get_data_from_yahoo(portfolio: list = None) -> None:
+    for stock in portfolio:
+        close_data = web.DataReader(stock, 'yahoo', start, end)['Close']
+        hist_data[stock] = close_data
 
+def get_data_from_ftx(portfolio: list = None) -> None:
+    api_conn = ftx_api.FtxApi(API_KEY, API_SECRET)
+    for crypto in portfolio:
+        name = f'{crypto}-{market_type}'
+        data = api_conn.get_hist_prices(name, 3600, start_ts, end_ts)
+        close_price = []
+        for hourly_data in data:
+            close_price.append(hourly_data['close'])
+        hist_data[name] = pd.Series(close_price)
+
+# get_data_from_yahoo(portfolio)
+get_data_from_ftx(portfolio)
 mean_pct_ret = hist_data.pct_change().mean()
 cov_matrix = hist_data.pct_change().cov()
 
 # portfolio simulations
-sim_count = 200000
+sim_count = 100000
 weights = []
 returns = []
 risks = []
@@ -71,5 +74,4 @@ plt.xlabel('Portfolio Risk')
 plt.ylabel('Portfolio Return')
 plt.colorbar(label='Sharpe Ratio')
 plt.scatter(risks[max_index], returns[max_index], c='red')
-plt.plot(returns, returns)
 plt.show()
